f054bbe031b7800b396a36369982337fe26c191b
Elijahgoh22 <142045191+Elijahgoh22@users.noreply.github.com>
2023-08-22 14:08:56 -0400

    Add files via upload

---
+++ firsttry.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(page_title="Strictly Me", page_icon=":🧑‍🚀:", layout="wide")
+
+#header section
+with st.container():
+    st.subheader("Hi, I am Elijah:👋:")
+    st.title("A Highschooler in North Carolina")
+    st.write("I am passionate about Computer Science, reading, movies and Lacrosse🥍")
+    st.write("[<This is my playlist>](https://open.spotify.com/playlist/0qOAXDlyX2gCoYYZu01cdW?si=464ab7f749a1461c)")
+
+
+
